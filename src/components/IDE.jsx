a077a4c9f195c233027a2dd36ecace0d1d06d065
deepakpixel <gtx.addicted@gmail.com>
2021-08-28 00:56:18 +0530

    Testing IDE (CodeMirror)

---
+++ src/components/IDE.jsx
@@ -0,0 +1,47 @@
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/keymap/sublime';
+
+import 'codemirror/addon/selection/active-line';
+
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchtags';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/comment/comment.js';
+
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/python/python';
+import 'codemirror/mode/javascript/javascript';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+
+const IDE = (props) => {
+  const { language, value, onChange } = props.options;
+
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+
+  return (
+    <ControlledEditor
+      onBeforeChange={handleChange}
+      value={value}
+      className="code-mirror-wrapper"
+      options={{
+        lineWrapping: true,
+        lint: true,
+        mode: language,
+        theme: 'material',
+        keyMap: 'sublime',
+        lineNumbers: true,
+        styleActiveLine: { nonEmpty: true },
+        matchBrackets: true,
+        matchTags: true,
+        autoCloseTags: true,
+        autoCloseBrackets: true,
+      }}
+    />
+  );
+};
+
+export default IDE;
