83c100f9ede4bb0053d5312e830d5a93bc918915
deepakpixel <gtx.addicted@gmail.com>
2021-09-02 17:25:38 +0530

    Disabled purge [dev] & added custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,27 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'slide-up': {
+          '0%': { transform: 'translateY(50%)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+        'slide-down': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        fade: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up .5s cubic-bezier(0, 1, 0, 1)',
+        'slide-down': 'slide-down .5s cubic-bezier(0, 1, 0, 1)',
+        fade: 'fade .5s cubic-bezier(0, 1, 0, 1)',
+      },
+    },
   },
   variants: {
     extend: {},
