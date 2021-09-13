41b8aee66a01bcb0d3c8877a8f1c1f8fd07e31fc
deepakpixel <gtx.addicted@gmail.com>
2021-09-14 00:21:18 +0530

    Added support to C++, Java, JS, Kotlin, Go

---
+++ src/config/DefaultCodeSnippets.js
@@ -0,0 +1,36 @@
+const DefaultCodeSnippets = {
+  cpp17: {
+    style: 'text/x-c++src',
+    code: '//Write your C code here\n',
+    version: 0,
+  },
+  nodejs: {
+    style: 'text/javascript',
+    code: '//Write your JS code here\n',
+    version: 3,
+  },
+  python3: {
+    style: 'text/x-python',
+    code: '#Write your PY code here\n',
+    version: 3,
+  },
+  java: {
+    style: 'text/x-java',
+    code: '//Write your JAVA code here\n',
+    version: 3,
+  },
+  go: {
+    style: 'text/x-go',
+    code: '//Write your GO code here\n',
+    version: 3,
+  },
+  kotlin: {
+    style: 'text/x-kotlin',
+    code: '//Write your GO code here\n',
+    version: 2,
+  },
+};
+
+//
+
+export default DefaultCodeSnippets;
